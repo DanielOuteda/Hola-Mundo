55fdceceb8aa2e659193791a993799f0c77043a8
doutedasolla <doutedasolla@gmail.com>
2019-11-08 14:20:13 +0100

    Salida saludo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hola Mundo");
+        System.out.println("vbf");
+        //TODO
+        //comprobar que el numero es negativo
     }
 }
